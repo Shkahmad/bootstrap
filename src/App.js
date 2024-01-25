92532c52879e55949d064f186a6e7b62ff904da6
Shkahmad <ahmadbahi4145@gmail.com>
2024-01-25 11:37:53 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './pages/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import Footer from './pages/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
